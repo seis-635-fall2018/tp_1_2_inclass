c46404bdade955b10edee7ba8d6ee60bd4ef3e97
evlevel <evlevel@stthomas.edu>
2018-10-05 18:17:46 -0500

    push to remote tp_1_2_inclass on seis-635-fall2018 GitHub rep

---
+++ SkunkProject/src/Scratch.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.introcs.*;
+
+public class Scratch
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		StdOut.print("hello");
+		System.out.println("hello");
+
+	}
+
+}
---
+++ SkunkProject/src/SkunkApp.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkApp
+{
+
+	public static void main(String[] args)
+	{
+		StdOut.println("Welcome to 635 Skunk project!");
+
+	}
+
+}
